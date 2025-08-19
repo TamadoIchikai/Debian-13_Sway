feee2652cad42a8aecdb50c341f2052c8454d028
TamadoIchikai <tamado.ichikai.kyooiku@gmail.com>
2025-08-20 02:55:47 +0700

    install rofi

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
-sudo apt install alacritty light sway swaybg swayidle swayimg swaylock waybar wofi llvm fonts-font-awesome build-essential libpam0g-dev libxcb-xkb-dev xauth xserver-xorg brightnessctl lightdm lightdm-gtk-greeter jq autotiling curl wget gpg flatpak
-mkdir -p ~/.config/sway/ ~/.config/waybar ~/.config/wofi 
+sudo apt install alacritty light sway swaybg swayidle swayimg swaylock waybar wofi llvm fonts-font-awesome build-essential libpam0g-dev libxcb-xkb-dev xauth xserver-xorg brightnessctl jq autotiling curl wget gpg flatpak meson ninja-build libcairo2-dev libpango1.0-dev libglib2.0-dev libgdk-pixbuf2.0-dev libxkbcommon-dev flex bison wayland-protocols libwayland-dev
+mkdir -p ~/.config/sway/ ~/.config/waybar ~/.config/rofi
+git clone https://github.com/davatorium/rofi.git ~/Downloads/Systems/
+meson setup ~/Downloads/Systems/rofi/build ~/Downloads/Systems/rofi -Dwayland=enabled -Dxcb=disabled
+sudo ninja -C ~/Downloads/Systems/rofi/build
 sudo sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /usr/local/bin
 chezmoi init https://github.com/TamadoIchikai/Debian-13_Sway
 chezmoi init --apply https://github.com/TamadoIchikai/Debian-13_Sway
@@ -8,7 +11,6 @@ wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /et
 echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
 sudo apt-get update && sudo apt-get install firefox 
 sudo apt install 
-sudo systemctl enable lightdm
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install flathub md.obsidian.Obsidian
 sudo reboot
