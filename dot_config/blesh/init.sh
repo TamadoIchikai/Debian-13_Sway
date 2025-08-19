ec6458f0f6778f8d16499ad1afbdb9f48d435d12
TamadoIchikai <tamado.ichikai.kyooiku@gmail.com>
2025-08-20 03:43:47 +0700

    add blesh config

---
+++ dot_config/blesh/init.sh
@@ -0,0 +1,7 @@
+ble-face auto_complete='fg=gray'
+ble-face region='bg=240'
+ble-face menu_filter_fixed='fg=cyan'
+ble-face syntax_command='fg=green,none'
+ble-face syntax_error='fg=red,bg=235,none'
+ble-bind -m auto_complete -f C-i auto_complete/insert
+ble-bind -m auto_complete -f TAB auto_complete/insert
