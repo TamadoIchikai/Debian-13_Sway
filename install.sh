3e9d79783d087c97b9e023c3f71f14e71f876b60
TamadoIchikai <tamado.ichikai.kyooiku@gmail.com>
2025-08-30 00:18:48 +0700

    Revert "change somthing idk"
    This reverts commit 36851db28d0bd14946f72c002d70815d19658a86.

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 sudo apt install alacritty light llvm fonts-font-awesome build-essential libpam0g-dev libxcb-xkb-dev xauth xserver-xorg brightnessctl jq autotiling curl wget gpg flatpak meson ninja-build libcairo2-dev libpango1.0-dev libglib2.0-dev libgdk-pixbuf2.0-dev libxkbcommon-dev flex bison gawk rofi rofi-dev libqalculate-dev qalc unzip dolphin konsole kio-extras kde-spectacle qt5ct kvantum vlc pipewire-audio wireplumber pipewire-pulse pipewire-jack libspa-0.2-bluetooth xdg-desktop-portal slurp copyq i3 xorg feh
 mkdir -p ~/.config/i3/ ~/.config/polybar ~/.config/rofi ~/Downloads/Systems ~/.local/bin
+=======
+sudo apt install alacritty light sway swaybg swayidle swayimg swaylock waybar wofi llvm fonts-font-awesome build-essential libpam0g-dev libxcb-xkb-dev xauth xserver-xorg brightnessctl jq autotiling curl wget gpg flatpak meson ninja-build libcairo2-dev libpango1.0-dev libglib2.0-dev libgdk-pixbuf2.0-dev libxkbcommon-dev flex bison wayland-protocols libwayland-dev gawk rofi rofi-dev libqalculate-dev qalc unzip dolphin konsole kio-extras kde-spectacle qt5ct kvantum vlc pipewire-audio wireplumber pipewire-pulse pipewire-jack libspa-0.2-bluetooth xdg-desktop-portal xdg-desktop-portal-wlr slurp copyq wl-clipboard grim wtype
+mkdir -p ~/.config/sway/ ~/.config/waybar ~/.config/rofi ~/Downloads/Systems
+>>>>>>> parent of 36851db (change somthing idk)
 systemctl --user mask pulseaudio.service pulseaudio.socket
 systemctl --user --now enable pipewire pipewire-pulse wireplumber.service
 git clone https://github.com/svenstaro/rofi-calc.git
@@ -28,8 +33,4 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 sudo flatpak install flathub md.obsidian.Obsidian
 flatpak install flathub io.missioncenter.MissionCenter
 flatpak override --user --socket=system-bus org.missioncenter.MissionCenter
-flatpak install flathub org.flameshot.Flameshot
-ln --symbolic /var/lib/flatpak/exports/bin/org.flameshot.Flameshot ~/.local/bin/flameshot
-echo 'export PATH="$PATH:$HOME/.local/bin"' >> .bashrc 
-source .bashrc 
 sudo reboot
