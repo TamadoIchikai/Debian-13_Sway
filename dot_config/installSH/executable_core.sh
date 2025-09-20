280d55cf29f8d3c1928e9dc5f0ae297a6d029f93
TamadoIchikai <130322347+TamadoIchikai@users.noreply.github.com>
2025-09-20 23:25:41 +0700

    Update executable_core.sh

--- dot_config/installSH/executable_core.sh
+++ dot_config/installSH/executable_core.sh
@@ -1,2 +1,2 @@
-sudo apt install light sway gtklock waybar autotiling curl wget gpg flatpak meson ninja-build wayland-protocols unzip jq xdg-utils
+sudo apt install light sway xwayland gtklock waybar autotiling curl wget gpg flatpak meson ninja-build wayland-protocols unzip jq xdg-utils
 
