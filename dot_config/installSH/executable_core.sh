bc10e4cdfca74e4edf3d67930fcdb86b0ab89240
TamadoIchikai <tamado.ichikai.kyooiku@gmail.com>
2025-09-28 18:30:12 +0700

    Change gtklock theme a little bit

--- dot_config/installSH/executable_core.sh
+++ dot_config/installSH/executable_core.sh
@@ -3,6 +3,6 @@ GREEN='\033[0;32m'
 NC='\033[0m' 
 
 echo -e "${BLUE}-------> Install core for my sway config ${NC}"
-sudo apt install light sway xwayland swaylock waybar autotiling curl wget gpg flatpak meson ninja-build wayland-protocols unzip jq xdg-utils
+sudo apt install light sway xwayland gtklock waybar autotiling curl wget gpg flatpak meson ninja-build wayland-protocols unzip jq xdg-utils
 echo -e "${GREEN}-------> DONE${NC}"
 
