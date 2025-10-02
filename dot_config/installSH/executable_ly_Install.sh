bad005b7c45762e18e2f3148aee05904775af96a
TamadoIchikai <130322347+TamadoIchikai@users.noreply.github.com>
2025-10-02 15:26:15 +0700

    Update executable_ly_Install.sh

--- dot_config/installSH/executable_ly_Install.sh
+++ dot_config/installSH/executable_ly_Install.sh
@@ -3,19 +3,19 @@ GREEN='\033[0;32m'
 NC='\033[0m' 
 
 echo -e "${BLUE}-------> install zig for ly ${NC}"
-wget -P ~/Downloads/Systems/ https://ziglang.org/download/0.15.1/zig-x86_64-linux-0.15.1.tar.xz
-tar -xf Downloads/Systems/zig-x86_64-linux-0.15.1.tar.xz -C ~/Downloads/Systems/
+cd ~/Downloads/Systems/
+wget https://ziglang.org/download/0.15.1/zig-x86_64-linux-0.15.1.tar.xz
+tar -xf Downloads/Systems/zig-x86_64-linux-0.15.1.tar.xz
 mv zig-x86_64-linux-0.15.1/ zig
-rm ~/Downloads/Systems/zig-x86_64-linux-0.15.1.tar.xz
-source ~/.bashrc
-echo "Zig version: $(zig -v)"
+rm zig-x86_64-linux-0.15.1.tar.xz
 sudo ln -s ~/Downloads/Systems/zig/zig /usr/local/bin/zig
+echo "Zig version: $(zig -v)"
 echo -e "${GREEN}-------> DONE${NC}"
 
 echo -e "${BLUE}-------> begin download ly and build the project ${NC}"
-git clone https://codeberg.org/fairyglade/ly.git ~/Downloads/Systems/ly
-cd ~/Downloads/Systems/ly
-zig build
+cd ~/Downloads/Systems/
+git clone https://codeberg.org/fairyglade/ly.git
+cd ly/
 sudo zig build installexe -Dinit_system=systemd
 sudo systemctl disable getty@tty2.service
 sudo systemctl enable ly.service
