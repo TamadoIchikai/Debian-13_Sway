c544a07a49f07d1344f783c26937154a392ee941
TamadoIchikai <130322347+TamadoIchikai@users.noreply.github.com>
2025-10-06 10:38:28 +0700

    Update executable_fonts.sh

--- dot_config/installSH/executable_fonts.sh
+++ dot_config/installSH/executable_fonts.sh
@@ -4,16 +4,13 @@ NC='\033[0m'
 
 set -e
 echo -e "${BLUE}-------> install nerd, awesome font alongside with papirus icon ${NC}"
-mkdir -p ~/.local/share/fonts
 sudo apt install fonts-font-awesome papirus-icon-theme fonts-noto-cjk
-cd ~/.local/share/fonts
-wget -O JetBrainsMono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/NerdFontsSymbolsOnly.zip
-unzip NerdFontsSymbolsOnly.zip
-unzip JetBrainsMono.zip
-rm NerdFontsSymbolsOnly.zip
-rm JetBrainsMono.zip
+wget -P ~/.local/share/fonts/ -O JetBrainsMono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
+wget -P ~/.local/share/fonts/ https://github.com/ryanoasis/nerd-fonts/releases/latest/download/NerdFontsSymbolsOnly.zip
+unzip ~/.local/share/fonts/NerdFontsSymbolsOnly.zip
+unzip ~/.local/share/fonts/JetBrainsMono.zip
+rm ~/.local/share/fonts/NerdFontsSymbolsOnly.zip
+rm ~/.local/share/fonts/JetBrainsMono.zip
 fc-cache -fv
-cd
 echo -e "${GREEN}-------> DONE${NC}"
 
